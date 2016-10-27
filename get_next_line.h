cfb90edeeaf275909811e0f21b7c413921e2149c
VitalyTen <vtenigin@student.42.us.org>
2016-10-26 18:42:07 -0700

    header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,6 +6,19 @@
 /*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/26 18:27:19 by vtenigin          #+#    #+#             */
-/*   Updated: 2016/10/26 18:27:21 by vtenigin         ###   ########.fr       */
+/*   Updated: 2016/10/26 18:41:18 by vtenigin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int get_next_line(const int fd, char **line);
+
+#endif
